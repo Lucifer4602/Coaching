6260c5a5935e11a7bb7d435dd2e225ff0dc4c160
Achyut Gupta <124287349+Lucifer4602@users.noreply.github.com>
2024-06-21 12:48:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const userRoutes = require("./routes/user");
-// const profileRoutes = require("./routes/profile");
-// const courseRoutes = require("./routes/courses");
-// const tagsRoutes = require("./routes/tag");
-// const cartRoutes = require("./routes/cart");
+const profileRoutes = require("./routes/profile");
+const courseRoutes = require("./routes/courses");
+const tagsRoutes = require("./routes/tag");
+const cartRoutes = require("./routes/cart");
 const fileUpload = require("express-fileupload");
 const { cloudinaryConnect } = require("./cloudinary");
 
@@ -41,10 +41,10 @@ app.use(
 cloudinaryConnect();
 
 app.use("/api/v1/auth", userRoutes);
-// app.use("/api/v1/profile", profileRoutes);
-// app.use("/api/v1/course", courseRoutes);
-// app.use("/api/v1/tags", tagsRoutes);
-// app.use("/api/v1/contact", contactUs);
-// app.use("/api/v1/cart", cartRoutes);
+app.use("/api/v1/profile", profileRoutes);
+app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/tags", tagsRoutes);
+app.use("/api/v1/contact", contactUs);
+app.use("/api/v1/cart", cartRoutes);
 
 db();

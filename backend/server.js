4891348835dadcea6e1a1b32d2dd1c23e55f6f46
Lucifer4602 <krishnamahajan4602@gmail.com>
2024-06-21 17:05:32 +0530

    render

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-// const userRoutes = require("./routes/user");
-// const profileRoutes = require("./routes/profile");
-// const courseRoutes = require("./routes/courses");
-// const tagsRoutes = require("./routes/tag");
-// const cartRoutes = require("./routes/cart");
-// const fileUpload = require("express-fileupload");
-// const { cloudinaryConnect } = require("./cloudinary");
+const userRoutes = require("./routes/user");
+const profileRoutes = require("./routes/profile");
+const courseRoutes = require("./routes/courses");
+const tagsRoutes = require("./routes/tag");
+const cartRoutes = require("./routes/cart");
+const fileUpload = require("express-fileupload");
+const { cloudinaryConnect } = require("./cloudinary");
 
 app.use(express.json());
 
@@ -14,40 +14,40 @@ app.listen(3000, () => {
   console.log("hello server is started haha");
 });
 
-// const db = require("./database");
-// const cookieParser = require("cookie-parser");
-// const cors = require("cors");
-// const { contactUs } = require("./controllers/contactUs");
+const db = require("./database");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const { contactUs } = require("./controllers/contactUs");
 
 app.use(express.json());
-// app.use(cookieParser());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.json("done");
 });
 
-// const corsConfig = {
-//   origin: "*",
-//   credential: true,
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-// };
-// app.options("", cors(corsConfig));
+const corsConfig = {
+  origin: "*",
+  credential: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.options("", cors(corsConfig));
 
-// app.use(cors(corsConfig));
+app.use(cors(corsConfig));
 
-// app.use(
-//   fileUpload({
-//     useTempFiles: true,
-//     tempFileDir: "/tmp",
-//   })
-// );
-// cloudinaryConnect();
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp",
+  })
+);
+cloudinaryConnect();
 
-// app.use("/api/v1/auth", userRoutes);
-// app.use("/api/v1/profile", profileRoutes);
-// app.use("/api/v1/course", courseRoutes);
-// app.use("/api/v1/tags", tagsRoutes);
-// app.use("/api/v1/contact", contactUs);
-// app.use("/api/v1/cart", cartRoutes);
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/profile", profileRoutes);
+app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/tags", tagsRoutes);
+app.use("/api/v1/contact", contactUs);
+app.use("/api/v1/cart", cartRoutes);
 
-// db();
+db();

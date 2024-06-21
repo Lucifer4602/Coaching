cb35722b666fdcf537890ada2d70501629de517b
Lucifer4602 <krishnamahajan4602@gmail.com>
2024-06-21 10:57:24 +0530

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,6 @@ const { contactUs } = require("./controllers/contactUs");
 app.use(express.json());
 app.use(cookieParser());
 
-<<<<<<< HEAD
 app.get("/", (req, res) => {
   res.json("done");
 });
@@ -35,21 +34,6 @@ const corsConfig = {
 app.options("", cors(corsConfig));
 
 app.use(cors(corsConfig));
-=======
-
-// Enable CORS for specific origins
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  })
-);
->>>>>>> origin
 
 app.use(
   fileUpload({
